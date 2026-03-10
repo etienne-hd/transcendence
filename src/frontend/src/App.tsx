6e0afd765d451ff721d8b8dc4e3f551464558ed5
nolanCrrd <nolan.crrd@gmail.com>
2026-03-10 16:14:46 +0100

    feat: api connexion

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,38 +1,60 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 import PageWrapper from "./components/PageWrapper";
+import { userService } from "./api/api.user";
+import type { User } from "./api/types/user";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [user, setUser] = useState<User | null>(null); // Initialisé à null
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        setLoading(true);
+        const data = await userService.getUserInfomation(1);
+        setUser(data);
+      } catch (error) {
+        console.error("Erreur API:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadUser();
+  }, []);
 
   return (
-    <>
-      <PageWrapper>
-        <div>
-          <a href="https://vite.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1 className="">Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-        <h1 className="text-3xl font-text">Hello World !</h1>
-      </PageWrapper>
-    </>
+    <PageWrapper>
+      <div className="card">
+        {loading ? (
+          <p>Chargement de l'utilisateur...</p>
+        ) : user ? (
+          <div className="bg-gray-100 p-4 rounded-lg text-black mb-4">
+            <p>
+              <strong>Nom:</strong> {user.name}
+            </p>
+            <p>
+              <strong>Username:</strong> {user.username}
+            </p>
+            <p>
+              <strong>Email:</strong> {user.email}
+            </p>
+          </div>
+        ) : (
+          <p>Aucun utilisateur trouvé.</p>
+        )}
+
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+
+      {!loading && (
+        <h1 className="text-3xl font-bold text-blue-600">Données reçues !</h1>
+      )}
+    </PageWrapper>
   );
 }
 
---
+++ src/frontend/src/api/api.service.ts
@@ -0,0 +1,20 @@
+import axios, { type AxiosInstance } from "axios";
+
+export const apiClient: AxiosInstance = axios.create({
+  baseURL: "http://127.0.0.1:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.request.use(
+  (config) => {
+    const jwtToken = localStorage.getItem("accessToken");
+    if (jwtToken) config.headers.Authorization = `Bearer ${jwtToken}`;
+    return config;
+  },
+  (error) => {
+    console.error("Request Interceptor Error:", error);
+    return Promise.reject(error);
+  },
+);
