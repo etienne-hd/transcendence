9263403ead089e26d92e8e55796a264edbb95f35
etienne-hd <hode.etienne@gmail.com>
2026-03-10 19:13:35 +0100

    feat(backend(auth)): added jwt/hash logic with login route

--- src/backend/src/module/auth/auth.controller.ts
+++ src/backend/src/module/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { BadRequestException, Controller, Post, Req } from '@nestjs/common';
+import { Controller, HttpCode, HttpStatus, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @HttpCode(HttpStatus.CREATED)
   @Post('/auth/register')
   postRegister(@Req() req: Request) {
     const body = req.body!;
@@ -15,5 +16,13 @@ export class AuthController {
       body['email'],
       body['name'],
     );
+  }
+
+  @HttpCode(HttpStatus.ACCEPTED)
+  @Post('/auth/login')
+  postLogin(@Req() req: Request) {
+    const body = req.body!;
+
+    return this.authService.login(body['username'], body['password']);
   }
 }
