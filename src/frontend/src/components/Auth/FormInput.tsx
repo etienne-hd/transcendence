49c284c4bf48d8227ecc2a077e0bc33e7c777da8
nolanCrrd <nolan.crrd@gmail.com>
2026-03-10 14:01:22 +0100

    style: form button outside

--- src/frontend/src/components/Auth/FormInput.tsx
+++ src/frontend/src/components/Auth/FormInput.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, type InputHTMLAttributes } from "react";
+import { forwardRef, type InputHTMLAttributes } from "react";
 import { type FieldError } from "react-hook-form";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -9,14 +9,14 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 const FormInput = forwardRef<HTMLInputElement, InputProps>(
   ({ label, error, ...props }, ref) => {
     return (
-      <div className="flex flex-col items-start">
+      <div className="flex flex-col items-start w-full">
         <label className="pl-1">{label}</label>
 
         <input
           ref={ref}
           {...props}
           className={
-            "border-2 p-2 rounded-main bg-bg-tertiary border-border-tertiary  focus:border-accent-primary outline-hidden invalid:border-error " +
+            "border-2 p-2 rounded-main w-full bg-bg-tertiary border-border-tertiary  focus:border-accent-primary outline-hidden invalid:border-error " +
             (error && "border-error")
           }
         />
--- src/frontend/src/components/Auth/RegisterForm.tsx
+++ src/frontend/src/components/Auth/RegisterForm.tsx
@@ -16,8 +16,8 @@ interface IRegisterInput {
 function RegisterForm() {
   const {
     register,
-    handleSubmit,
     formState: { errors },
+    handleSubmit,
   } = useForm<IRegisterInput>();
 
   // TODO: API call for submition
@@ -30,80 +30,88 @@ function RegisterForm() {
   };
 
   // TODO: OAuth connexion
+  // TODO: link to general terms
   return (
-    <div className="flex flex-col gap-4 md:flex-row bg-bg-secondary border-border-secondary border-2 p-padding-main rounded-main justify-center items-center shadow-xl ">
-      <div className="flex justify-center items-center md:w-md">
-        <h1>Welcome</h1>
-      </div>
-
-      <form
-        className="flex flex-col justify-center items-center gap-gap-main w-full"
-        onSubmit={handleSubmit(onSubmit, onError)}
-      >
-        <FormInput
-          {...register("name", {
-            required: { value: true, message: "Name required" },
-            maxLength: { value: 20, message: "Name too long (20 max)" },
-          })}
-          label="Name"
-          type="text"
-          placeholder="Toto"
-          error={errors.name}
-        />
-
-        <FormInput
-          {...register("username", {
-            required: { value: true, message: "Username required" },
-            maxLength: { value: 20, message: "Username too long (20 max)" },
-          })}
-          label="Username"
-          type="text"
-          placeholder="xXTotoXx"
-          error={errors.username}
-        />
-
-        <FormInput
-          {...register("email", {
-            required: { value: true, message: "Email required" },
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: "Enter a valid email format",
-            },
-          })}
-          label="Email"
-          type="email"
-          placeholder="xXTotoXx@gmail.com"
-          error={errors.email}
-        />
-
-        <FormInput
-          {...register("password", {
-            required: { value: true, message: "Password Required" },
-            minLength: { value: 8, message: "Minimum lenght: 8" },
-          })}
-          label="Password"
-          type="password"
-          placeholder="Your best secret"
-          error={errors.password}
-        />
-
-        <div className="flex flex-row gap-2 ">
-          <input
-            {...register("conditions", {
-              required: { value: true, message: "Accept our generals terms" },
-            })}
-            type="checkbox"
-          />
-          <label className={errors.conditions && "text-error"}>
-            General terms of utilisation
-          </label>
+    <div className="flex flex-col justify-center items-center gap-6">
+      <div className="flex flex-col gap-4 md:flex-row bg-bg-secondary border-border-secondary border-2 p-padding-main rounded-main justify-center items-center shadow-xl ">
+        <div className="flex flex-col gap-2 justify-center items-center md:w-sm">
+          <h1 className="text-4xl font-tech">Welcome</h1>
+          <h1 className="text-2xl font-tech">To</h1>
+          <h1 className="text-4xl font-tech">Unicord</h1>
         </div>
 
-        <input
-          type="submit"
-          className="rounded-main p-2 mt-4 bg-accent-primary hover:bg-accent-secondary hover:scale-105 transition-all duration-200 text-bg-secondary"
-        />
-      </form>
+        <form
+          className="flex flex-col justify-center items-center gap-gap-main w-md"
+          onSubmit={handleSubmit(onSubmit, onError)}
+          id="register"
+        >
+          <FormInput
+            {...register("name", {
+              required: { value: true, message: "Name required" },
+              maxLength: { value: 20, message: "Name too long (20 max)" },
+            })}
+            label="Name"
+            type="text"
+            placeholder="Toto"
+            error={errors.name}
+          />
+
+          <FormInput
+            {...register("username", {
+              required: { value: true, message: "Username required" },
+              maxLength: { value: 20, message: "Username too long (20 max)" },
+            })}
+            label="Username"
+            type="text"
+            placeholder="xXTotoXx"
+            error={errors.username}
+          />
+
+          <FormInput
+            {...register("email", {
+              required: { value: true, message: "Email required" },
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Enter a valid email format",
+              },
+            })}
+            label="Email"
+            type="email"
+            placeholder="xXTotoXx@gmail.com"
+            error={errors.email}
+          />
+
+          <FormInput
+            {...register("password", {
+              required: { value: true, message: "Password Required" },
+              minLength: { value: 8, message: "Minimum lenght: 8" },
+            })}
+            label="Password"
+            type="password"
+            placeholder="Your best secret"
+            error={errors.password}
+          />
+
+          <div className="flex flex-row gap-2 ">
+            <input
+              {...register("conditions", {
+                required: { value: true, message: "Accept our generals terms" },
+              })}
+              type="checkbox"
+            />
+            <label className={errors.conditions && "text-error"}>
+              General terms of utilisation
+            </label>
+          </div>
+        </form>
+      </div>
+      <button
+        type="submit"
+        className="rounded-main p-2 border-2 border-border-secondary text-md bg-bg-secondary hover:bg-bg-tertiary hover:scale-105 transition-all duration-200 "
+        form="register"
+      >
+        Register in Unicord
+      </button>
     </div>
   );
 }
