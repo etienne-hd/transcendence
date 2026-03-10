a8f24b08ed475a4c325e45ae54664246c190c709
etienne-hd <hode.etienne@gmail.com>
2026-03-10 22:37:14 +0100

    feat(backend(user)): added basic get user / edit user logic

--- src/backend/src/module/user/user.service.ts
+++ src/backend/src/module/user/user.service.ts
@@ -1,10 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserEntity } from './user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
-  getUser(id: String) {
-    return {
-      user_id: id,
-    };
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  public async getUser(id: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOneBy({
+      id: id,
+    });
+
+    if (!user) {
+      throw new NotFoundException('Unable to find user');
+    }
+
+    return user;
+  }
+
+  public async editUser(id: number, fields: JSON): Promise<UserEntity> {
+    const editableFields = [
+      'username',
+      'email',
+      'name',
+      'biography',
+      'password',
+      'avatar',
+    ];
+
+    const user = await this.getUser(id);
+
+    for (const field in fields) {
+      if (editableFields.includes(field)) {
+        user[field] = fields[field];
+      }
+    }
+
+    await this.userRepository.save(user);
+
+    return user;
   }
 }
