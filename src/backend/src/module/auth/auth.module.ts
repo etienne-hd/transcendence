731084b4d3398a167a127ecec55bb396b514919d
etienne-hd <hode.etienne@gmail.com>
2026-03-10 19:13:04 +0100

    feat(backend(auth)): added jwt config

--- src/backend/src/module/auth/auth.module.ts
+++ src/backend/src/module/auth/auth.module.ts
@@ -3,9 +3,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserEntity } from '../user/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET', '1234'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
