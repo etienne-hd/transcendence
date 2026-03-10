4468e6325c829a2da259ef1e927fb395dcf1190c
etienne-hd <hode.etienne@gmail.com>
2026-03-10 14:47:16 +0100

    feat(backend): backend is connected to the database

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppUserService } from './service/user';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // charge .env
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mariadb',
+        host: 'database',
+        port: 3306,
+        username: 'root',
+        password: config.get('MYSQL_ROOT_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppUserService],
 })
