c7c204b2ee3b14f667b4407c12fec205f86ba6cc
nolanCrrd <contact@nolancrrd.fr>
2026-03-12 10:06:47 +0100

    refactor: modal component because user mutli time

--- src/frontend/src/components/Friends/AddFriendModal.tsx
+++ src/frontend/src/components/Friends/AddFriendModal.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useFriends } from "../../context/FriendListContext";
+import Modal from "../Modal";
 
 interface AddFriendModalProps {
   toggleFriend: () => void;
@@ -25,52 +26,44 @@ function AddFriendModal(props: AddFriendModalProps) {
   }, [props]);
 
   return (
-    <div
-      onClick={() => {
+    <Modal
+      toggleModal={() => {
         props.toggleFriend();
       }}
-      className="fixed flex justify-center items-center top-0 left-0 z-20 w-full h-full bg-black/40 backdrop-blur-xs"
     >
-      <div
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-        className="bg-bg-tertiary p-4 max-h-[70%] flex flex-col gap-4 w-200 overflow-y-scroll rounded-xl"
-      >
-        <div className="bg-bg-secondary flex flex-row gap-6 justify-between items-center rounded-lg p-4 w-full">
-          <p className="font-bold w-full">Enter the username of your friend</p>
-          <input
-            type="text"
-            placeholder="Jean"
-            className={
-              "border-2 p-2 rounded-main w-full bg-bg-tertiary border-border-tertiary  focus:border-accent-primary outline-hidden invalid:border-error "
-            }
-            onChange={(e) => {
-              setFriend(e.target.value);
-            }}
-          />
-        </div>
-        <div className="flex flex-row gap-4 justify-end items-center">
-          <button
-            onClick={() => {
-              props.toggleFriend();
-            }}
-            className="p-2 bg-error w-fit rounded-md hover:scale-102 hover:shadow-xl duration-200 cursor-pointer"
-          >
-            <p>Cancel</p>
-          </button>
-          <button
-            onClick={() => {
-              if (friend != undefined) addFriend(friend);
-            }}
-            disabled={friend == undefined || friend == ""}
-            className="p-2 bg-accent-primary disabled:cursor-auto disabled:hover:scale-100 disabled:hover:shadow-none disabled:bg-white/5 w-fit rounded-md hover:scale-102 hover:shadow-xl duration-200 cursor-pointer"
-          >
-            <p>Save changes</p>
-          </button>
-        </div>
+      <div className="bg-bg-secondary flex flex-row gap-6 justify-between items-center rounded-lg p-4 w-full">
+        <p className="font-bold w-full">Enter the username of your friend</p>
+        <input
+          type="text"
+          placeholder="Jean"
+          className={
+            "border-2 p-2 rounded-main w-full bg-bg-tertiary border-border-tertiary  focus:border-accent-primary outline-hidden invalid:border-error "
+          }
+          onChange={(e) => {
+            setFriend(e.target.value);
+          }}
+        />
       </div>
-    </div>
+      <div className="flex flex-row gap-4 justify-end items-center">
+        <button
+          onClick={() => {
+            props.toggleFriend();
+          }}
+          className="p-2 bg-error w-fit rounded-md hover:scale-102 hover:shadow-xl duration-200 cursor-pointer"
+        >
+          <p>Cancel</p>
+        </button>
+        <button
+          onClick={() => {
+            if (friend != undefined) addFriend(friend);
+          }}
+          disabled={friend == undefined || friend == ""}
+          className="p-2 bg-accent-primary disabled:cursor-auto disabled:hover:scale-100 disabled:hover:shadow-none disabled:bg-white/5 w-fit rounded-md hover:scale-102 hover:shadow-xl duration-200 cursor-pointer"
+        >
+          <p>Save changes</p>
+        </button>
+      </div>
+    </Modal>
   );
 }
 
--- src/frontend/src/components/Settings/SettingsModal.tsx
+++ src/frontend/src/components/Settings/SettingsModal.tsx
@@ -4,6 +4,7 @@ import UserInformation from "./UserInformation";
 import axios from "axios";
 import { userService } from "../../api/api.user";
 import { useNotification } from "../../context/NotificationContext";
+import Modal from "../Modal";
 
 interface SettingsModalProps {
   toggleSettings: () => void;
@@ -70,111 +71,101 @@ function SettingsModal(props: SettingsModalProps) {
   }, [props]);
 
   return (
-    <div
+    <Modal
       onClick={() => {
         props.toggleSettings();
       }}
-      className="fixed flex justify-center items-center top-0 left-0 z-20 w-full h-full bg-black/40 backdrop-blur-xs"
     >
-      <div
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-        className="bg-bg-tertiary p-4 max-h-[70%] flex flex-col gap-4 w-200 overflow-y-scroll rounded-xl"
-      >
-        <div className="bg-bg-secondary flex flex-col gap-6 justify-center items-center rounded-lg p-4 w-full">
-          <div className="flex flex-row gap-6 justify-between p-4 items-center w-full">
-            <div className="flex flex-row gap-6 justify-center items-center">
-              <div className="relative h-full">
-                <img
-                  src={user?.avatar ? user.avatar : "placehoder"}
-                  className="rounded-full h-20 w-20"
-                />
-                <div
-                  className={
-                    "absolute bottom-0 right-0 w-4 h-4 rounded-full " +
-                    (isActive ? "bg-green-500" : "bg-error")
-                  }
-                ></div>
-              </div>
-              <div className="flex flex-col justify-center items-start">
-                <h1 className="text-xl font-semibold">{user?.username}</h1>
-                <h2 className="text-md text-font-secondary">{user?.name}</h2>
-              </div>
-            </div>
-            <div className="w-1/2 h-full bg-bg-tertiary rounded-md flex p-2 flex-col gap-2 justify-center items-start">
-              <div className="flex flex-row justify-between items-center w-full">
-                <p className="font-semibold">Biography</p>
-              </div>
-              <textarea
-                onChange={(e) => {
-                  setBiography(e.target.value);
-                }}
-                placeholder={
-                  user?.biography
-                    ? user.biography
-                    : "Write your own biography ?"
-                }
+      <div className="bg-bg-secondary flex flex-col gap-6 justify-center items-center rounded-lg p-4 w-full">
+        <div className="flex flex-row gap-6 justify-between p-4 items-center w-full">
+          <div className="flex flex-row gap-6 justify-center items-center">
+            <div className="relative h-full">
+              <img
+                src={user?.avatar ? user.avatar : "placehoder"}
+                className="rounded-full h-20 w-20"
+              />
+              <div
                 className={
-                  "border-2 p-1 rounded-main w-full bg-bg-tertiary border-border-tertiary  focus:border-accent-primary outline-hidden invalid:border-error "
+                  "absolute bottom-0 right-0 w-4 h-4 rounded-full " +
+                  (isActive ? "bg-green-500" : "bg-error")
                 }
-              ></textarea>
+              ></div>
+            </div>
+            <div className="flex flex-col justify-center items-start">
+              <h1 className="text-xl font-semibold">{user?.username}</h1>
+              <h2 className="text-md text-font-secondary">{user?.name}</h2>
             </div>
           </div>
-          <div className="w-[90%] h-1 bg-bg-tertiary rounded-full" />
-          <div className="flex flex-col gap-6 w-full justify-center items-center">
-            <UserInformation
-              label="Name"
-              value={user?.name ? user.name : "undefined"}
-              onChange={setName}
-            />
-
-            <UserInformation
-              label="Username"
-              value={user?.username ? user.username : "undefined"}
-              onChange={setUsername}
-            />
-
-            <UserInformation
-              label="Email"
-              value={user?.email ? user.email : "undefined"}
-              onChange={setEmail}
-            />
-
-            <UserInformation
-              label="Password"
-              value={"********"}
-              onChange={setPassword}
-            />
+          <div className="w-1/2 h-full bg-bg-tertiary rounded-md flex p-2 flex-col gap-2 justify-center items-start">
+            <div className="flex flex-row justify-between items-center w-full">
+              <p className="font-semibold">Biography</p>
+            </div>
+            <textarea
+              onChange={(e) => {
+                setBiography(e.target.value);
+              }}
+              placeholder={
+                user?.biography ? user.biography : "Write your own biography ?"
+              }
+              className={
+                "border-2 p-1 rounded-main w-full bg-bg-tertiary border-border-tertiary  focus:border-accent-primary outline-hidden invalid:border-error "
+              }
+            ></textarea>
           </div>
         </div>
-        <div className="w-full flex flex-row justify-end items-center gap-6">
-          <button
-            onClick={() => {
-              props.toggleSettings();
-            }}
-            className="p-2 bg-error w-fit rounded-md hover:scale-102 hover:shadow-xl duration-200 cursor-pointer"
-          >
-            <p>Cancel</p>
-          </button>
-          <button
-            onClick={saveChange}
-            disabled={
-              !(
-                (name != undefined && name != "") ||
-                (username != undefined && username != "") ||
-                (email != undefined && email != "") ||
-                (password != undefined && password != "") ||
-                (biography != undefined && biography != "")
-              )
-            }
-            className="p-2 bg-accent-primary disabled:hover:scale-100 disabled:hover:shadow-none disabled:bg-white/5 w-fit rounded-md hover:scale-102 hover:shadow-xl duration-200 cursor-pointer"
-          >
-            <p>Save changes</p>
-          </button>
+        <div className="w-[90%] h-1 bg-bg-tertiary rounded-full" />
+        <div className="flex flex-col gap-6 w-full justify-center items-center">
+          <UserInformation
+            label="Name"
+            value={user?.name ? user.name : "undefined"}
+            onChange={setName}
+          />
+
+          <UserInformation
+            label="Username"
+            value={user?.username ? user.username : "undefined"}
+            onChange={setUsername}
+          />
+
+          <UserInformation
+            label="Email"
+            value={user?.email ? user.email : "undefined"}
+            onChange={setEmail}
+          />
+
+          <UserInformation
+            label="Password"
+            value={"********"}
+            onChange={setPassword}
+          />
         </div>
       </div>
-    </div>
+      <div className="w-full flex flex-row justify-end items-center gap-6">
+        <button
+          onClick={() => {
+            props.toggleSettings();
+          }}
+          className="p-2 bg-error w-fit rounded-md hover:scale-102 hover:shadow-xl duration-200 cursor-pointer"
+        >
+          <p>Cancel</p>
+        </button>
+        <button
+          onClick={saveChange}
+          disabled={
+            !(
+              (name != undefined && name != "") ||
+              (username != undefined && username != "") ||
+              (email != undefined && email != "") ||
+              (password != undefined && password != "") ||
+              (biography != undefined && biography != "")
+            )
+          }
+          className="p-2 bg-accent-primary disabled:hover:scale-100 disabled:hover:shadow-none disabled:bg-white/5 w-fit rounded-md hover:scale-102 hover:shadow-xl duration-200 cursor-pointer"
+        >
+          <p>Save changes</p>
+        </button>
+      </div>
+    </Modal>
   );
 }
 
