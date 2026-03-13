39592746a153bcd531d40bbe79d036edbd593b43
etienne-hd <hode.etienne@gmail.com>
2026-03-13 14:24:34 +0100

    feat(backend): put all value length max/min, regex into a constants.ts

--- src/backend/src/module/friend/dtos/delete-friend.dtos copy.ts
+++
@@ -1,7 +0,0 @@
-import * as z from 'zod';
-
-export const DeleteFriendSchema = z.object({
-  username: z.string().min(3).max(30),
-});
-
-export type DeleteFriendDto = z.infer<typeof DeleteFriendSchema>;
--- src/backend/src/module/user/dtos/put-me.dtos.ts
+++ src/backend/src/module/user/dtos/put-me.dtos.ts
@@ -1,16 +1,45 @@
+import {
+  USER_BIOGRAPHY_MAX_LENGTH,
+  USER_BIOGRAPHY_MIN_LENGTH,
+  USER_EMAIL_MAX_LENGTH,
+  USER_EMAIL_MIN_LENGTH,
+  USER_EMAIL_REGEX,
+  USER_NAME_MAX_LENGTH,
+  USER_NAME_MIN_LENGTH,
+  USER_PASSWORD_MAX_LENGTH,
+  USER_PASSWORD_MIN_LENGTH,
+  USER_USERNAME_MIN_LENGTH,
+} from 'src/common/constants/constants';
 import * as z from 'zod';
 
 export const PutMeSchema = z.object({
-  username: z.string().min(3).max(30).optional(),
-  password: z.string().min(8).max(128).optional(),
+  username: z
+    .string()
+    .min(USER_USERNAME_MIN_LENGTH)
+    .max(USER_PASSWORD_MAX_LENGTH)
+    .optional(),
+  password: z
+    .string()
+    .min(USER_PASSWORD_MIN_LENGTH)
+    .max(USER_PASSWORD_MAX_LENGTH)
+    .optional(),
   email: z
     .string()
-    .min(3)
-    .max(320)
-    .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    .min(USER_EMAIL_MIN_LENGTH)
+    .max(USER_EMAIL_MAX_LENGTH)
+    .regex(USER_EMAIL_REGEX)
     .optional(),
-  name: z.string().min(1).max(100).optional(),
-  biography: z.string().min(1).max(200).optional().nullable(),
+  name: z
+    .string()
+    .min(USER_NAME_MIN_LENGTH)
+    .max(USER_NAME_MAX_LENGTH)
+    .optional(),
+  biography: z
+    .string()
+    .min(USER_BIOGRAPHY_MIN_LENGTH)
+    .max(USER_BIOGRAPHY_MAX_LENGTH)
+    .optional()
+    .nullable(),
 });
 
 export type PutMeDto = z.infer<typeof PutMeSchema>;
