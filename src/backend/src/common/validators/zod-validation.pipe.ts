cce9e28a4c4be163946503ca9d916e99dda47667
etienne-hd <hode.etienne@gmail.com>
2026-03-13 19:36:24 +0100

    feat(backend): added specific message when zod validation message failed

--- src/backend/src/common/validators/zod-validation.pipe.ts
+++ src/backend/src/common/validators/zod-validation.pipe.ts
@@ -9,12 +9,7 @@ export class ZodValidationPipe implements PipeTransform {
     const parsed = this.schema.safeParse(value);
 
     if (!parsed.success) {
-      const issues = parsed.error.issues;
-
-      const message =
-        issues.length > 0
-          ? `${issues[0].path.join('.')}: ${issues[0].message}`
-          : 'Invalid body';
+      const message = parsed.error.issues[0]?.message || 'Invalid body';
 
       throw new BadRequestException({
         message: message,
--- src/backend/src/common/validators/zod-validation.rule.ts
+++ src/backend/src/common/validators/zod-validation.rule.ts
@@ -18,27 +18,62 @@ import {
 // User
 export const ZOD_USER_USERNAME = z
   .string()
-  .min(USER_USERNAME_MIN_LENGTH)
-  .max(USER_USERNAME_MAX_LENGTH)
-  .regex(USER_USERNAME_REGEX);
+  .min(USER_USERNAME_MIN_LENGTH, {
+    message: `Username too short! (${USER_USERNAME_MIN_LENGTH} min)`,
+  })
+  .max(USER_USERNAME_MAX_LENGTH, {
+    message: `Username too long! (${USER_USERNAME_MAX_LENGTH} max)`,
+  })
+  .regex(USER_USERNAME_REGEX, {
+    message: 'The username may only contain lowercase letters and digits.',
+  });
 export const ZOD_USER_PASSWORD = z
   .string()
-  .min(USER_PASSWORD_MIN_LENGTH)
-  .max(USER_PASSWORD_MAX_LENGTH)
-  .describe('Test');
+  .min(USER_PASSWORD_MIN_LENGTH, {
+    message: `Password too short! (${USER_PASSWORD_MIN_LENGTH} min)`,
+  })
+  .max(USER_PASSWORD_MAX_LENGTH, {
+    message: `Password too long! (${USER_PASSWORD_MAX_LENGTH} min)`,
+  })
+  .regex(/.*[a-z].*/, {
+    message: 'Password must contains at least one lower-case character!',
+  })
+  .regex(/.*[A-Z].*/, {
+    message: 'Password must contains at least one upper-case character!',
+  })
+  .regex(/.*\d.*/, {
+    message: 'Password must contains at least one number!',
+  })
+  .regex(/.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?].*/, {
+    message: 'Password must contains at least one special character!',
+  });
 export const ZOD_USER_EMAIL = z
   .string()
-  .min(USER_EMAIL_MIN_LENGTH)
-  .max(USER_EMAIL_MAX_LENGTH)
-  .regex(USER_EMAIL_REGEX);
+  .min(USER_EMAIL_MIN_LENGTH, {
+    message: `Email too short! (${USER_EMAIL_MIN_LENGTH} min)`,
+  })
+  .max(USER_EMAIL_MAX_LENGTH, {
+    message: `Email too long! (${USER_EMAIL_MAX_LENGTH} max)`,
+  })
+  .regex(USER_EMAIL_REGEX, { message: 'please provide a valid email!' });
 export const ZOD_USER_NAME = z
   .string()
-  .min(USER_NAME_MIN_LENGTH)
-  .max(USER_NAME_MAX_LENGTH);
+  .min(USER_NAME_MIN_LENGTH, {
+    message: `Name too short! (${USER_NAME_MIN_LENGTH} min)`,
+  })
+  .max(USER_NAME_MAX_LENGTH, {
+    message: `Name too long! (${USER_NAME_MAX_LENGTH} max)`,
+  });
 export const ZOD_USER_BIOGRAPHY = z
   .string()
-  .min(USER_BIOGRAPHY_MIN_LENGTH)
-  .max(USER_BIOGRAPHY_MAX_LENGTH);
+  .min(USER_BIOGRAPHY_MIN_LENGTH, {
+    message: `Biography too short! (${USER_BIOGRAPHY_MIN_LENGTH} min)`,
+  })
+  .max(USER_BIOGRAPHY_MAX_LENGTH, {
+    message: `Biography too long! (${USER_BIOGRAPHY_MAX_LENGTH} max)`,
+  });
 
 // Message
-export const ZOD_MESSAGE_CONTENT = z.string().max(MESSAGE_CONTENT_MAX_LENGTH);
+export const ZOD_MESSAGE_CONTENT = z.string().max(MESSAGE_CONTENT_MAX_LENGTH, {
+  message: `Message too long! (${MESSAGE_CONTENT_MAX_LENGTH} max)`,
+});
