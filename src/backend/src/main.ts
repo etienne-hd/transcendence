fa5ab4b9b704378afb0d0bef326e61d28dba8589
etienne-hd <hode.etienne@gmail.com>
2026-03-21 14:56:49 +0100

    feat: added https for backend

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -1,18 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import * as fs from 'node:fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { rawBody: true });
+  const httpsOptions = {
+    key: fs.readFileSync('./secrets/private-key.pem'),
+    cert: fs.readFileSync('./secrets/public-certificate.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+    httpsOptions,
+  });
 
   app.enableCors({
     origin: ['http://127.0.0.1:3001', 'https://unicord.fr'],
     exposedHeaders: ['Content-Length'],
-    
   });
   app.use(bodyParser.json({ limit: '10mb' }));
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 443);
 }
 bootstrap();
