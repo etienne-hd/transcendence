93c3552d2e56e908f28bbfb89853f7965732faa3
nolanCrrd <contact@nolancrrd.fr>
2026-03-24 11:45:35 +0100

    feat(notification): Notification on message and friend request

--- src/frontend/src/context/MessageContext.tsx
+++ src/frontend/src/context/MessageContext.tsx
@@ -179,6 +179,7 @@ function MessageContextProvider(props: MessageContextProviderProps) {
         getMessage();
       } else {
         updateFriends();
+        pushNotification("New message received", "notif");
       }
     };
 
